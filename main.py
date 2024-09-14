ebbb64ce1e32be578e7a31b15aca5b37fc8fc8c3
Hayyan67 <humairabaig81@gmail.com>
2024-09-14 15:05:23 +0500

    initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+def add(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+def mul(a,b):
+    return a*b
+def div(a,b):
+    return a/b
+x=int(input('Enter the first number'))
+y=int(input('Enter the second number'))
+print('Addition is: ',add(x,y))
+print('Subtraction is: ',sub(x,y))
+print('MUltiplication is: ',mul(x,y))
+print('Division is: ',div(x,y))
